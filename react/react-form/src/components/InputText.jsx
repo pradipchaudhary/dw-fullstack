aaefed1c47078ca0478201e7d3924a87e5d5d019
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-09-02 00:59:20 +0545

    basic react form

--- react/react-form/src/components/InputText.jsx
+++ react/react-form/src/components/InputText.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 
 const InputText = () => {
-    const [username, setUsername] = useState("");
-    console.log(username);
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    console.log(firstName);
 
     // Submit handler
 
@@ -10,25 +11,41 @@ const InputText = () => {
         console.log("Submit handler...");
         e.preventDefault();
         console.log("On submitHandler ...!");
-        console.log("username: ", username);
+        console.log("FirstName: ", firstName);
+        console.log("LastName: ", lastName);
     };
     return (
         <div>
             <form onSubmit={submitHandler} className="mt-4">
                 <label
-                    htmlFor="username"
+                    htmlFor="firstname"
                     className="block text-sm font-medium text-slate-700 py-2"
                 >
-                    Username
+                    First Name
                 </label>
                 <input
                     type="text"
-                    value={username}
+                    value={firstName}
                     onChange={(e) => {
-                        setUsername(e.target.value);
+                        setFirstName(e.target.value);
                     }}
                     className="mt-1 block w-full bg-white border border-slate-300 rounded-md px-3 py-2"
                 />
+                <label
+                    htmlFor="lastname"
+                    className="block py-2 text-sm font-medium text-slate-700"
+                >
+                    Last Name
+                </label>
+                <input
+                    type="text"
+                    value={lastName}
+                    onChange={(e) => {
+                        setLastName(e.target.value);
+                    }}
+                    className="mt-1 block w-full bg-white border border-slate-300 rounded-md py-2 px-3"
+                />
+
                 <button
                     className="bg-sky-500 text-white py-2 px-4 mt-4 rounded-md"
                     type="submit"
