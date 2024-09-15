b4b30b3a1b4a3677cce20acb63b51281fc5fce4c
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-09-16 01:06:25 +0545

    update readme.md

--- node/express2/src/connectTodb/connectToDb.js
+++ node/express2/src/connectTodb/connectToDb.js
@@ -1,16 +1,14 @@
 import mongoose from "mongoose";
 import { dbUrl } from "../../constant.js";
 
-// const connectToMongdoDb = () => {
-//   mongoose.connect("mongodb://localhost:27017/dw17project");
-// };
 const connectToMongdoDb = async () => {
-  try {
-    await mongoose.connect(`${dbUrl}`);
-    console.log(`application is connected to successfully at port ${dbUrl}`);
-  } catch (error) {
-    console.log(error.message);
-  }
+    try {
+        await mongoose.connect(`${dbUrl}`);
+        console.log(`MongoDB connection successfully.`);
+    } catch (error) {
+        console.error(error.message);
+        process.exit(1);
+    }
 };
 
 export default connectToMongdoDb;
