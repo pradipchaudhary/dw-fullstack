8d074b9ab03b1df1099b915d9acd86074b541584
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-09-16 03:12:32 +0545

    add admin routes

--- node/express2/src/controller/productController.js
+++ node/express2/src/controller/productController.js
@@ -1,10 +1,10 @@
 import expressAsyncHandler from "express-async-handler";
 import {
-  createProductService,
-  deleteProductService,
-  readAllProductService,
-  readSpecificProductService,
-  updateProductService,
+    createProductService,
+    deleteProductService,
+    readAllProductService,
+    readSpecificProductService,
+    updateProductService,
 } from "../service/productService.js";
 
 import { EventEmitter } from "events";
@@ -14,60 +14,60 @@ const myEvent = new EventEmitter();
 let count = 0;
 
 myEvent.on("countApi", () => {
-  //creating an event
-  count++;
-  console.log(count);
+    //creating an event
+    count++;
+    console.log(count);
 });
 
 export let createProductController = expressAsyncHandler(
-  async (req, res, next) => {
-    let result = await createProductService(req);
-    res.status(200).json({
-      success: true,
-      message: "post succesfully created ",
-      result: result,
-    });
-  }
+    async (req, res, next) => {
+        let result = await createProductService(req);
+        res.status(200).json({
+            success: true,
+            message: "post succesfully created ",
+            result: result,
+        });
+    }
 );
 
 export let readAllProductController = expressAsyncHandler(
-  async (req, res, next) => {
-    myEvent.emit("countApi");
-    let result = await readAllProductService(req);
-    res.status(200).json({
-      success: true,
-      message: "Product read successfully",
-      result: result,
-    });
-  }
+    async (req, res, next) => {
+        myEvent.emit("countApi");
+        let result = await readAllProductService(req);
+        res.status(200).json({
+            success: true,
+            message: "Product read successfully",
+            result: result,
+        });
+    }
 );
 export let readSpecificProductController = expressAsyncHandler(
-  async (req, res, next) => {
-    let result = await readSpecificProductService(req);
-    res.status(200).json({
-      success: true,
-      message: "Product get successfuly",
-      result: result,
-    });
-  }
+    async (req, res, next) => {
+        let result = await readSpecificProductService(req);
+        res.status(200).json({
+            success: true,
+            message: "Product get successfuly",
+            result: result,
+        });
+    }
 );
 export let upadteProductController = expressAsyncHandler(
-  async (req, res, next) => {
-    let result = await updateProductService(req);
-    res.status(200).json({
-      success: true,
-      message: "Product updated successfully",
-      result: result,
-    });
-  }
+    async (req, res, next) => {
+        let result = await updateProductService(req);
+        res.status(200).json({
+            success: true,
+            message: "Product updated successfully",
+            result: result,
+        });
+    }
 );
 export let deleteProductController = expressAsyncHandler(
-  async (req, res, next) => {
-    let result = await deleteProductService(req);
-    res.status(200).json({
-      success: true,
-      message: "Product Deleted Succesfully",
-      result: result,
-    });
-  }
+    async (req, res, next) => {
+        let result = await deleteProductService(req);
+        res.status(200).json({
+            success: true,
+            message: "Product Deleted Succesfully",
+            result: result,
+        });
+    }
 );
