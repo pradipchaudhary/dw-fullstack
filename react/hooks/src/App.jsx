167124398c6a6ad3836dd7aac4b1f5a93b99f1d8
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-10-09 20:14:36 +0545

    explore react hooks...

---
+++ react/hooks/src/App.jsx
@@ -0,0 +1,12 @@
+import FetchData from "./components/FetchData";
+
+function App() {
+    return (
+        <div>
+            <h1>App</h1>
+            <FetchData />
+        </div>
+    );
+}
+
+export default App;
