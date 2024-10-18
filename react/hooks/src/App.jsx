428e10a3021c2cd3975c98aa0a48e2e6ffa42258
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-10-18 15:02:38 +0545

    React hooks explore...

---
+++ react/hooks/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+const App = () => {
+    return (
+        <div>
+            <h1>Todo lists</h1>
+            <hr />
+        </div>
+    );
+};
+
+export default App;
